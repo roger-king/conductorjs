2e5ebfd420e452c78a086ed6bea23f2a0205d032
Roger King <rogr.king@gmail.com>
2020-08-15 12:01:56 -0400

    feat: installation and dbconnection to context

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
+# Still in Development
+
 # lightning-bolt-js
+
 A middleware for slack bolt-js framework to help manage your sessions and installations
+
+## Development
+
+```bash
+    yarn install
+    yarn link
+
+    # starts a tsc -w
+    yarn dev
+
+
+```
+
+## Usage
+
+```javascript
+    // In your app instance
+    installationStore: {
+        storeInstallation: async (installation) => {
+            return await lb.saveInstallation(installation.team.id, installation);
+        },
+        fetchInstallation: async (InstallQuery) => {
+            return await lb.fetchInstallation(InstallQuery.teamId);
+        },
+    },
+
+    .
+    .
+    .
+    // Adds additional information to your context
+    this.app.use((args) => lb.listen({ ...args }));
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-export const hello = "hello";
+import { Tedis } from "tedis";
+import { Installation } from "@slack/oauth";
+
+export interface DBConnectionConfig {
+  host: string;
+  port?: string;
+  password?: string;
+  timeout?: string;
+}
+
+export interface ILightningBolt {}
+
+export class LightningBolt implements ILightningBolt {
+  public dbConnection: Tedis;
+  public message: any;
+  public next: any;
+
+  constructor(dbConfig: DBConnectionConfig) {
+    this.dbConnection = new Tedis(dbConfig as any);
+  }
+
+  private async setDbContext(context: any) {
+    context.db = {
+      connection: this.dbConnection,
+    };
+  }
+
+  public async set(key: string, value: any) {
+    if (typeof value === "object") {
+      value = JSON.stringify(value);
+    }
+
+    this.dbConnection.set(key, value);
+  }
+
+  public async get(key: string): Promise<any> {
+    return this.dbConnection.get(key);
+  }
+
+  public async saveInstallation(teamId: string, installation: Installation) {
+    this.set(teamId, installation);
+  }
+
+  public async fetchInstallation(id: string) {
+    const installation: Installation = await this.get(id);
+    return installation;
+  }
+
+  public async listen({ payload, context, next }: any) {
+    this.setDbContext(context);
+    await next();
+  }
+}
